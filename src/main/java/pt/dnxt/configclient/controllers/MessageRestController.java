ceabf05060af33f37e64453a6f7976e1e95c8c35
Antonio Pedro Santos <aps@digitalnext.pt>
2022-02-18 16:20:37 +0000

    add standalone profile and management endpoints

--- src/main/java/pt/dnxt/configclient/controllers/MessageRestController.java
+++ src/main/java/pt/dnxt/configclient/controllers/MessageRestController.java
@@ -1,19 +1,49 @@
 package pt.dnxt.configclient.controllers;
 
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
 
 @RefreshScope
-@Controller
+@RestController
+@Slf4j
+@EnableConfigurationProperties
+@ConfigurationProperties
+@Setter
 public class MessageRestController {
 
     @Value("${message:Hello default}")
     private String message;
 
+    // @Value("#{${imt_messages}}")
+    private Map<String, String> imtMessages;
+
     @GetMapping("/message")
     public String getMessage() {
         return this.message;
+    }
+
+    @GetMapping("/errorMessage")
+    public ResponseEntity<String> getErrorMessage(@RequestParam(name = "msg") String msg) {
+        log.debug("Message: " + msg);
+        log.debug(imtMessages != null ? imtMessages.toString() : "<<<>>>");
+
+        return this.imtMessages != null ? ResponseEntity.ok(this.imtMessages.get(msg))
+                : ResponseEntity.notFound().build();
+    }
+
+    @ConfigurationProperties("imt_messages")
+    public Map<String, String> getImtMessages() {
+        return this.imtMessages;
     }
 }
